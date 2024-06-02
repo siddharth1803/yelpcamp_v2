c3535b782252da80467fea7d5c1f795489c3d434
siddharth <shobhitsingh1803@gmail.com>
2024-06-02 09:59:46 +0530

    cookie change

--- yelpcamp-backend/app.js
+++ yelpcamp-backend/app.js
@@ -59,7 +59,7 @@ const sessionConfig = {
     saveUninitialized: true,
     cookie: {
         httpOnly: true,
-        secure: process.env.NODE_ENV === 'production',
+        secure: true,
         sameSite: "None",
         expires: Date.now() + 1000 * 60 * 60 * 24 * 7,
         maxAge: 1000 * 60 * 60 * 24 * 7,
